ca7d52eec4b2a04ccbefa83e3e3659935522caca
Abr <59525997+Abr009@users.noreply.github.com>
2024-03-03 13:14:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function redirectToRegistration() {
-    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSebxXRqXTiBYSy77rbgYlqqw99jF0buI-JC7Aat1yK45_ITjQ/viewform";
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfvg90PtO9LT3RGCjHrO33Qi2MTUvstxZZW6Rbs5xxePeqDsQ/viewform";
 }
 
 function displaytoggle(item) {
